6eab92b66bfed4e4f71399ce60bc8e1ac59a6766
cedric karungu <ckarungu921@gmail.com>
2022-03-04 17:23:29 +0200

    ready to remote

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -6,13 +6,35 @@ const BurgerIngredient = (props) => {
   let ingredient = null;
 
   switch (props.type) {
-    case ('bread-bottom'):
-       ingredient = <div className={classes.BreadBottom}></div>
-      break;
-  
+    case 'bread-bottom':
+      ingredient = <div className={classes.BreadBottom}></div>
+      break
+    case 'bread-top':
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1}></div>
+          <div className={classes.Seeds2}></div>
+        </div>
+      )
+      break
+    case 'meat':
+      ingredient = <div className={classes.Meat}></div>
+      break
+    case 'cheese':
+      ingredient = <div className={classes.Chees}></div>
+      break
+    case 'bacon':
+      ingredient = <div className={classes.Bacon}></div>
+      break
+    case 'salad':
+      ingredient = <div className={classes.Salad}></div>
+      break
+
     default:
+      ingredient = null;
       break;
   }
+  return ingredient;
 };
 
 export default BurgerIngredient;
